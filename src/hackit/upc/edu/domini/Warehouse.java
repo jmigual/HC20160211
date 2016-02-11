981f4798b7cc5a81eccf5a34a8f9ed65df519139
Aleix P <Aleix P>
2016-02-11 22:00:23 +0100

    ADI

--- src/hackit/upc/edu/domini/Warehouse.java
+++ src/hackit/upc/edu/domini/Warehouse.java
@@ -1,20 +1,13 @@
 package hackit.upc.edu.domini;
 
-import java.util.ArrayList;
-
 /**
  * Created by Joan on 11/02/2016.
  */
 public class Warehouse {
     public Inventory inventory;
     private Point position;
-    public int puntuation;
-    public ArrayList<Drone> drones;
 
     public Warehouse(Point position) {
         this.position = position;
-        puntuation = -1;
-        drones = new ArrayList<>();
-        inventory = new Inventory();
     }
 }

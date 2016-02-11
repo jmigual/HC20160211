bca4c52849021ae8ab0429cce14a83881ae2ca0f
Aleix P <Aleix P>
2016-02-11 20:53:31 +0100

    Limits

--- src/hackit/upc/edu/domini/Point.java
+++ src/hackit/upc/edu/domini/Point.java
@@ -7,6 +7,7 @@ package hackit.upc.edu.domini;
 public class Point {
     public Integer x;
     public Integer y;
+    private static Point limits;
 
     public static Integer distance(Point a, Point b){
         int distance;
@@ -16,13 +17,23 @@ public class Point {
         return distance;
     }
 
+    public static Point getLimits() {
+        return limits;
+    }
+
+    public static void setLimits(Point limits) {
+        Point.limits = limits;
+    }
+
     public Point(){
         x = -1;
         y = -1;
     }
 
-    public Point(Integer x, Integer y){
-        this.x = x;
-        this.y = y;
+    public Point(Integer x, Integer y) throws Exception{
+        if(x < limits.x && x >= 0) this.x = x;
+        else throw new Exception("Limits");
+        if(y < limits.y && y >= 0) this.y = y;
+        else throw new Exception("Limits");
     }
 }

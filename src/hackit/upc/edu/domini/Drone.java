60247ab7782d7aaeed66baef237198374033e617
jmigual <j.marce.igual@gmail.com>
2016-02-11 19:42:32 +0100

    First created classes

---
+++ src/hackit/upc/edu/domini/Drone.java
@@ -0,0 +1,13 @@
+package hackit.upc.edu.domini;
+
+import java.util.HashMap;
+
+/**
+ * Created by Joan on 11/02/2016.
+ */
+public class Drone {
+
+    private static int capacity;
+    private HashMap<ProductType, Integer> inventory;
+    private Point position;
+}
